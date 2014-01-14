05e46a871c679c0d3b29d01fc96ee1e6e1854296
aikons <dugujianke@live.com>
2014-01-14 17:10:31 +0800

    Port Mokee statusbar traffic (2/2)
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Change-Id: I8d9544ebf0a82d6b035457fa2c30bf90ac100815

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4685,6 +4685,12 @@ public final class Settings {
         public static final String DIALER_DIRECT_CALL = "dialer_direct_call";
 
         /**
+         * show traffic in statusbar. The value is
+         * int (0: Hide  or 1: Only Rx 2: Rx&Tx).
+         */
+        public static final String STATUS_BAR_TRAFFIC_STYLE = "status_bar_traffic_style";
+
+        /**
          * Should Flip to Silence be used
          *
          * @hide
