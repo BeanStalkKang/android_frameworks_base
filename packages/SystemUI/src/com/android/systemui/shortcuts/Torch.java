SlimRoms/frameworks_base

SystemUI: Slim Actions to Shortcuts (1/2)
Change-Id: Ic7cc70b5a55e98b1fd6784380049f54d0bd3f4d2

packages/SystemUI/src/com/android/systemui/shortcuts/Torch.java
@@ -19,9 +19,9 @@ package com.android.systemui.shortcuts;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.UserHandle;
 
-import com.android.internal.util.slim.ButtonsConstants;
-import com.android.internal.util.slim.SlimActions;
+import com.android.internal.util.slim.TorchConstants;
 
 public class Torch extends Activity  {
 
@@ -33,8 +33,21 @@ public class Torch extends Activity  {
     @Override
     public void onResume() {
         super.onResume();
-        SlimActions.processAction(
-                this, ButtonsConstants.ACTION_TORCH, false);
+        int value = getIntent().getIntExtra("value", 2);
+        Intent intent = new Intent();
+        switch (value) {
+            case 0:
+                intent.setAction(TorchConstants.ACTION_OFF);
+                break;
+            case 1:
+                intent.setAction(TorchConstants.ACTION_ON);
+                break;
+            case 2:
+                intent.setAction(TorchConstants.ACTION_TOGGLE_STATE);
+                break;
+        }
+        this.sendBroadcastAsUser(
+                intent, new UserHandle(UserHandle.USER_CURRENT));
         this.finish();
     }
 }

BeanStalkKang/android_frameworks_base

Signed-off-by: scott <scott@ubuntu>
Conflicts:
	packages/SystemUI/src/com/android/systemui/shortcuts/PowerMenu.java
	packages/SystemUI/src/com/android/systemui/shortcuts/Torch.java

packages/SystemUI/src/com/android/systemui/shortcuts/Torch.java
@@ -19,9 +19,9 @@ package com.android.systemui.shortcuts;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.UserHandle;
 
-import com.android.internal.util.beanstalk.ButtonsConstants;
-import com.android.internal.util.beanstalk.SlimActions;
+import com.android.internal.util.beanstalk.TorchConstants;
 
 public class Torch extends Activity  {
 
@@ -33,8 +33,21 @@ public class Torch extends Activity  {
     @Override
     public void onResume() {
         super.onResume();
-        SlimActions.processAction(
-                this, ButtonsConstants.ACTION_TORCH, false);
+        int value = getIntent().getIntExtra("value", 2);
+        Intent intent = new Intent();
+        switch (value) {
+            case 0:
+                intent.setAction(TorchConstants.ACTION_OFF);
+                break;
+            case 1:
+                intent.setAction(TorchConstants.ACTION_ON);
+                break;
+            case 2:
+                intent.setAction(TorchConstants.ACTION_TOGGLE_STATE);
+                break;
+        }
+        this.sendBroadcastAsUser(
+                intent, new UserHandle(UserHandle.USER_CURRENT));
         this.finish();
     }
 }